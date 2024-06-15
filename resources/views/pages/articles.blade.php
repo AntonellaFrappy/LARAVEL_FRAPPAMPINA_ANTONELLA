abbebee880a1c17e92fd7020334d00f122740516
Antonella Frappampina <antonella.frappy@live.it>
2024-06-15 19:15:46 +0200

    esercitazioni wee

--- resources/views/pages/articles.blade.php
+++ resources/views/pages/articles.blade.php
@@ -3,26 +3,73 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> {{ $title }} </title>
-    <link rel="stylesheet" href="{{asset ('css/style.css')}}">
+    <title>{{$title}}</title>
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 <body>
-  <nav>
-    <a href =" {{ route ('welcome')}}">Home </a>
-    <a href =" {{ route ('articles')}}">Articoli </a>
-    <a href =" {{ route ('about-us')}}">Chi siamo </a>
-    <a href =" {{ route ('contacts')}}"> Contatti </a>
+    
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="nav-link active" aria-current="page" href="{{ route('welcome') }}">Home</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <a class="nav-link" href="{{ route('articles') }}">Articoli</a>
+            <a class="nav-link" href="{{ route('aboutUs') }}">Chi Siamo</a>
+            <a class="nav-link" href="{{ route('contacts') }}">Contatti</a>
+        </div>
+        </div>
+    </div>
     </nav>
-    @if($titleIsVisible)
-    <h1 class="title">Articoli</h1>
+
+    <h1>{{$title}}</h1>
+
+    <div class="card">
+    <div class="card-body">
+        
+
+    @if ($articles)
+
+
+    
+    @foreach($articles as $index => $article)
+        @if($article['visible'])
+        <li>
+            <a href="{{ route('articles.show', $index) }}">{{ $article['title'] . ' - ' . $article['category'] }}</a>
+        </li>
+
+        @endif
+    @endforeach
+
+        @else
+
+
+        <p>Nessun articolo disponibile</p>
+
+
     @endif
 
-    @foreach($articles as $index => $article)
-    @if ($article['visible'])
-    <li>
-      <a href="{{route ('article.show',$index)}}">{{index}} - {($article ['title'])}
-    </li>
-    @endif
-    @endforeach
+
+
+
+    </div>
+    </div>
+
+
+    
+   
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    
+    
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,63 +1,17 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('welcome');
 
-Route::get('/articoli', function () {
 
-    $title = 'Articoli';
+Route::get('/', [PageController::class, 'welcome'])->name("welcome");
 
-    $titleIsVisible = true;
+Route::get('/articoli', [PageController::class, 'articles'])->name("articles");
 
-    $articles = [
-        0 => ['title' => 'Titolo articolo #1', 'category' => 'Sport #1', 'visible' => true],
-        1 => ['title' => 'Titolo articolo #2', 'category' => 'Sport #2', 'visible' => true],
-        2 => ['title' => 'Titolo articolo #3', 'category' => 'Sport #3', 'visible' => true],
-        3 => ['title' => 'Titolo articolo #4', 'category' => 'Sport #4', 'visible' => true],
-    ];
+Route::get('/articoli/{id}', [PageController::class, 'articles'])->name('articles.show');
 
-    //$articles = [];
+Route::get('/contatti', [PageController::class, 'contacts'])->name("contacts");
 
-    return view('pages.articles', [
-        'titleIsVisible' => $titleIsVisible,
-        'title' => $title,
-        'articles' => $articles,
-    ]);
-
-})->name('articles');
-
-Route::get('/articoli/{id}', function ($id=null) {
-
-    
-    $articles = [
-        0 => ['title' => 'Titolo articolo #1', 'category' => 'Sport #1', 'description' => '...', 'visible' => true],
-        1 => ['title' => 'Titolo articolo #2', 'category' => 'Sport #2', 'description' => '...', 'visible' => true],
-        2 => ['title' => 'Titolo articolo #3', 'category' => 'Sport #3', 'description' => '...', 'visible' => true],
-        3 => ['title' => 'Titolo articolo #4', 'category' => 'Sport #4', 'description' => '...', 'visible' => true],
-    ];
-
-    $article = $articles[$id];
-
-    return view('pages.article', ['article' => $article]);
-
-})->name('articles.show');
-
-Route::get('/contatti', function () {
-
-    return view('pages.contacts');
-
-})->name('contacts');
-
-Route::get('/chi-siamo', function () {
-
-    $description = 'Descrizione della pagina Chi Siamo';
-
-    return view('pages.about-us', [
-        'title' => 'Chi Siamo',
-        'description' => $description,
-    ]);
-
-})->name('about-us');
+Route::get('/chi-siamo', [PageController::class, 'aboutUs'])->name("aboutUs");
